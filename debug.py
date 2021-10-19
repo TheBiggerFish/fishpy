4aac393ac71ae53f2062be791f43a5913ba5ebf4
TheBiggerFish <clhnumber4@gmail.com>
2021-10-19 15:36:59 -0400

    Updating debug for pylint

--- debug.py
+++ debug.py
@@ -1,59 +1,60 @@
+"""This module contains function decorators to use in debugging"""
+
 # Shamelessly stolen from https://tech.serhatteker.com/post/2019-07/python-debug-decorators/
 
-import functools
-import time
 import cProfile
+import functools
 import io
 import pstats
+import time
+from typing import Callable
 
-# logging.basicConfig(level=logging.DEBUG)
-# logger = logging.getLogger(__name__)
 
 def monitor(func):
-    """ Debug a method and return it back """
-
+    """ Debug a method, printing inputs and ouputs """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        
         return_value = func(*args, **kwargs)
         print(f'Calling: {func.__name__}')
         print(f'Function args: {args}, kwargs: {kwargs}')
         print(f'Function returned {return_value}')
-        # logger.debug(f'Calling : {func.__name__}')
-        # logger.debug(f'args, kwarg: {args, kwargs}')
-        # logger.debug(f'{func.__name__} returned {return_value}')
-
         return return_value
-
     return wrapper
+
+def log(log_function:Callable[[str],None]=print):
+    """ Debug a method and output using log_function """
+    def decorator(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            return_value = function(*args, **kwargs)
+            log_function(f'Calling: {function.__name__}')
+            log_function(f'Function args: {args}, kwargs: {kwargs}')
+            log_function(f'Function returned: {return_value}')
+            return return_value
+        return wrapper
+    return decorator
 
 def timer(func):
     """ Calculate the execution time of a method and return it back """
-
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
         duration = round(time.time() - start,6)
-
         print(f'Duration of {func.__name__} function was {duration}')
-        # logger.debug(f'Duration of {func.__name__} function was {duration}')
         return result
     return wrapper
 
 def profile(func):
     """ Profile the execution of a method """
-
     def wrapper(*args, **kwargs):
-        # datafn = func.__name__ + '.profile' # Name the data file sensibly
         prof = cProfile.Profile()
         prof.enable()
         retval = func(*args,**kwargs)
-        # retval = prof.runcall(func, *args, **kwargs)
         prof.disable()
-        s = io.StringIO()
-        ps = pstats.Stats(prof, stream=s).sort_stats('cumtime')
-        ps.print_stats()
-        print(s.getvalue())
+        stream = io.StringIO()
+        stats = pstats.Stats(prof, stream=stream).sort_stats('cumtime')
+        stats.print_stats()
+        print(stream.getvalue())
         return retval
     return wrapper
