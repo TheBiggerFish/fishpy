eb9d3e1e39aec8981d6def40b207cdd7b01367fe
TheBiggerFish <clhnumber4@gmail.com>
2021-10-19 16:05:53 -0400

    Updating modulo module for pylint

--- modulo.py
+++ modulo.py
@@ -1,21 +1,29 @@
+"""Functions related to modular arithmetic"""
+
+from math import prod
 from typing import List
+
 from sympy.ntheory.factor_ import totient
-from math import prod 
+
 
 # https://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Using_Euler's_theorem
 def modular_inverse(n:int,base:int) -> int:
+    """Returns the modular inverse of n in base"""
     n **= totient(base) - 1
     n %= base
     return n
 
 # https://crypto.stanford.edu/pbc/notes/numbertheory/crt.html
 def chinese_remainder_theorem(a:List[int],m:List[int]) -> int:
-    assert len(a) == len(m)
+    """Performs the chinese remainder theorem on recieved lists"""
+
+    if len(a) != len(m):
+        raise ValueError('Length of a must match length of m')
 
     M = prod(m)
     solution = 0
-    for i in range(len(a)):
-        b = M // m[i]
-        b_inv = modular_inverse(b,m[i])
+    for i,m_i in enumerate(a):
+        b = M // m_i
+        b_inv = modular_inverse(b,m_i)
         solution += a[i] * b * b_inv
     return solution % M
