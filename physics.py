5fe6190abe766ab549b4b012df652d6237042995
TheBiggerFish <clhnumber4@gmail.com>
2021-11-10 23:36:36 -0500

    Updated physics.py to pass linting

--- physics.py
+++ physics.py
@@ -1,7 +1,12 @@
+"""
+This module provides a class to represent physics objects
+"""
+
 from .geometry import Point, Vector
 
 
 class Object:
+    """This class is used to represent objects with physics"""
     def __init__(self,position:Point=Point(0,0),
                  velocity:Vector=Vector(0,0),
                  acceleration:Vector=Vector(0,0),
@@ -12,6 +17,8 @@ class Object:
         self._update_position_first = update_position_first
 
     def step(self) -> None:
+        """Step forward one unit of time, updating position and velocity"""
+
         if self._update_position_first:
             self.position += self.velocity
             self.velocity += self.acceleration
