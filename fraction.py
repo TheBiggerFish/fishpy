443b0d212e2e03b2b20e71b178528dfd335c3f4e
TheBiggerFish <clhnumber4@gmail.com>
2021-11-11 09:16:48 -0500

    Updated fraction.py to pass linting Pylint

--- fraction.py
+++ fraction.py
@@ -1,32 +1,52 @@
 """Structures to be used in evaluating fractions and continued fractions"""
 
 #pylint: disable=invalid-name
+
 from math import gcd
 from typing import Optional, Sequence
 
 
 class Fraction:
+    """Class to be used in storing and evaluating fractions"""
+
     def __init__(self,numer,denom=1):
         self.n = numer
         self.d = denom
 
     def is_proper(self) -> bool:
+        """Predicate method that returns whether the function is proper"""
         return self.n < self.d
 
-    def evaluate(self) -> bool:
+    def evaluate(self) -> float:
+        """Return the actual value of of the fraction"""
         return self.n / self.d
 
     def reciprocal(self) -> 'Fraction':
+        """Return the reciprocal fraction of self"""
         return Fraction(self.d,self.n)
 
     def add_int(self,num:int) -> 'Fraction':
+        """Add an integer value to the fraction"""
         return Fraction(self.n + (num * self.d),self.d)
+
+    def reduce(self) -> 'Fraction':
+        """Reduce the fraction to the lowest equivalent value"""
+        div = gcd(self.n,self.d)
+        return Fraction(self.n//div,self.d//div)
+
+    @staticmethod
+    def _lcd(f1:'Fraction',f2:'Fraction'):
+        return Fraction._lcm(f1.d,f2.d)
+
+    @staticmethod
+    def _lcm(a:int,b:int) -> int:
+        return abs(a*b) // gcd(a,b)
 
     def __mul__(self,other:'Fraction') -> 'Fraction':
         return Fraction(self.n*other.n,self.d*other.d)
 
     def __add__(self,other:'Fraction') -> 'Fraction':
-        lcd = Fraction.lcd(self,other)
+        lcd = Fraction._lcd(self,other)
         self_n = self.n * (lcd // self.d)
         other_n = other.n * (lcd // other.d)
         return Fraction(self_n + other_n,lcd)
@@ -43,24 +63,14 @@ class Fraction:
     def __eq__(self,other:'Fraction') -> bool:
         return self.evaluate() == other.evaluate()
 
-    @staticmethod
-    def __lcm(a:int,b:int) -> int:
-        return abs(a*b) // gcd(a,b)
-
-    @staticmethod
-    def lcd(f1:'Fraction',f2:'Fraction'):
-        return Fraction.__lcm(f1.d,f2.d)
-
-    def reduce(self) -> 'Fraction':
-        div = gcd(self.n,self.d)
-        return Fraction(self.n//div,self.d//div)
-
     def __hash__(self) -> int:
         if self.n >= self.d:
             return self.n * self.n + self.n + self.d
         return self.d * self.d + self.n
 
 class ContinuedFraction:
+    """Class for storing continued fractions"""
+
     def __init__(self,addend:int=0,numer:int=0,
                  denom:Optional['ContinuedFraction']=None):
         self.a = addend
@@ -68,9 +78,12 @@ class ContinuedFraction:
         self.d = denom
 
     @staticmethod
-    # Generate continued fraction using a continued fraction representation sequence.
-    # The value x is used to generate the numerators of the continued fraction
     def gen_from_seq(seq:Sequence[int],x:int=2) -> 'ContinuedFraction':
+        """
+        Generate a continued fraction from a sequence of integers
+        The value x is used to generate the numerators of the continued fraction
+        """
+
         if len(seq) == 0:
             return ContinuedFraction(addend=0)
 
@@ -83,8 +96,9 @@ class ContinuedFraction:
             frac.a = num
         return frac
 
-    # Determines the representation sequence of a continued fraction
     def get_seq(self) -> Sequence[int]:
+        """Returns the sequence of integers that represents a continued fraction"""
+
         seq = [self.a]
         frac = self.d
         while frac is not None:
@@ -92,8 +106,9 @@ class ContinuedFraction:
             frac = frac.d
         return seq
 
-    # Adds a continued fraction to the end of a continued fraction. 
     def append(self,frac:'ContinuedFraction') -> 'ContinuedFraction':
+        """Append continued fraction to the last continued fraction"""
+
         recurse = self
         while recurse.d is not None:
             recurse = recurse.d
@@ -101,20 +116,26 @@ class ContinuedFraction:
         return self
 
     def evaluate(self) -> float:
+        """Recursively determine the value of the continued fraction"""
+
         if self.n == 0 or self.d is None:
             return float(self.a)
         return self.a + (self.n / self.d.evaluate())
 
     def reduce_fraction(self) -> Fraction:
+        """Recursively evaluate the continued fraction to find a representative fraction"""
         if self.n == 0 or self.d is None:
             return Fraction(self.a)
         sub_fraction = Fraction(self.n) * self.d.reduce_fraction().reciprocal()
         return sub_fraction.add_int(self.a)
 
     @staticmethod
-    # Generate a continued fraction representation sequence of
-    #   length l for a square root of number n
     def gen_sqrt_seq(n:int,l:int) -> Sequence[int]:
+        """
+        Generate a continued fraction representation sequence of length l for
+        a square root of number n
+        """
+
         if (n**0.5).is_integer():
             return [n**0.5]
         m = 0
@@ -129,18 +150,25 @@ class ContinuedFraction:
         return seq
 
     def copy(self) -> 'ContinuedFraction':
+        """Returns a shallow copy of self"""
         return ContinuedFraction(addend=self.a,numer=self.n,denom=self.d)
 
     @property
     def a(self) -> int:
+        """Represents the addend of the continued fraction"""
         return self.__a
 
     @property
     def n(self) -> int:
+        """Represents the numerator of the continued fraction"""
         return self.__n
 
     @property
     def d(self) -> 'ContinuedFraction':
+        """
+        Represents the denominator of the continued fraction,
+        another continued fraction
+        """
         return self.__d
 
     @a.setter
