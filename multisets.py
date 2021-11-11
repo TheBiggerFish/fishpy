b0670bd994ff2fdcaa083f0db05c1d3f3c5c28c6
TheBiggerFish <clhnumber4@gmail.com>
2021-11-10 23:43:35 -0500

    Updated multisets.py to pass linting

--- multisets.py
+++ multisets.py
@@ -1,20 +1,29 @@
+"""
+This module provides a set of functions related to multisets.
+"""
+
 from multiset import Multiset
 
 from .primes import is_prime
 
 
 def multiset_prime_factors(n,primes=None):
+    """Find the smallest subset of prime factors using multisets"""
+
+    # If n is prime, return a multiset containing only n
     if (primes is None and is_prime(n)) or (primes is not None and n in primes):
         return Multiset([n])
+
+    # Find the first divisor of n, add the prime multisets containing factors of the divisors
     for i in range(2,int(n**0.5)+1):
         if n % i == 0:
-            return multiset_prime_factors(i) + multiset_prime_factors(int(n/i))
+            return multiset_prime_factors(i) + multiset_prime_factors(n//i)
     return Multiset([])
 
-def multiset_factors(n,primes=None):
-    f = Multiset()
-    for i in range(1,int(n**0.5)+1):
-        if n%i == 0:
-            f.add(i,n//i)
-            f.add(n//i,i)
-    return f
+# def multiset_factors(n,primes=None):
+#     f = Multiset()
+#     for i in range(1,int(n**0.5)+1):
+#         if n%i == 0:
+#             f.add(i,n//i)
+#             f.add(n//i,i)
+#     return f
