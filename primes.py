6892158e1d3b906144661e9974e1e5a3b9e8d7eb
TheBiggerFish <clhnumber4@gmail.com>
2021-11-10 23:16:31 -0500

    Updated primes.py to pass linting

--- primes.py
+++ primes.py
@@ -1,9 +1,19 @@
-from math import prod,gcd
+"""
+This module provides a set of functions related to prime numbers.
+"""
+
+# pylint: disable=invalid-name
+
+from math import gcd
 from typing import Set
+
 from sympy.ntheory.factor_ import totient
 
+
 def is_prime(n:int) -> bool:
-    if n == 2 or n == 3:
+    """Iteratively check every possible divisor to determine if n is prime"""
+
+    if n in (2,3):
         return True
     if n % 2 == 0 or n % 3 == 0 or n <= 1:
         return False
@@ -13,20 +23,24 @@ def is_prime(n:int) -> bool:
     return True
 
 def factors(n:int) -> Set[int]:
+    """Iteratively check every possible divisor to find the factors of n"""
+
     f = set()
     for i in range(1,int(n**0.5)+1):
         if n%i == 0:
             f.add(i)
             f.add(n//i)
     return f
-    
+
 def prime_factors(n:int) -> Set[int]:
+    """Iteratively check every possible divisor to find prime factors of n"""
+
     f = set()
     if n % 2 == 0:
         f.add(2)
         while n % 2 == 0:
             n //= 2
-    
+
     for i in range(3,int(n**0.5),2):
         if n % i == 0:
             f.add(i)
@@ -37,6 +51,8 @@ def prime_factors(n:int) -> Set[int]:
     return f
 
 def sieve(max_:int) -> Set[int]:
+    """Use Eratosthenes' sieve to find all prime numbers smaller than max_"""
+
     composite = set()
     for i in range(2,int(max_**0.5)+2):
         if i in composite:
@@ -45,13 +61,19 @@ def sieve(max_:int) -> Set[int]:
             composite.add(j)
     return set(range(2,max_)).difference(composite)
 
-def are_coprime(n1:int,n2) -> bool:
+def are_coprime(n1:int,n2:int) -> bool:
+    """Predicate function to determine if 2 numbers are coprime"""
+
     if n1 % n2 == 0 or n2 % n1 == 0:
         return False
     return gcd(n1,n2) == 0
 
 # https://math.stackexchange.com/questions/1854197/calculate-the-number-of-integers-in-a-given-interval-that-are-coprime-to-a-given
 def range_totient(n:int,max_:int,min_:int=2) -> int:
+    """
+    Calculate the count of numbers in an interval which are coprime to n
+    """
+
     full_range = int(max_-min_-((max_-min_)%n))
     rt = int(round(totient(n)/n * full_range,12))
     new_min = min_ + full_range + 1
