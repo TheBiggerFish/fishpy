0494e41f516be1352070ade6856dc36190c32376
TheBiggerFish <clhnumber4@gmail.com>
2021-11-15 15:12:08 -0500

    Added docstring to pathfinding submodule

--- pathfinding/__init__.py
+++ pathfinding/__init__.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring
+"""This module contains a collection of classes to aid in pathfinding"""
 
 from .depthfirst import DepthFirstTraversal
 from .dijkstra import Dijkstra
