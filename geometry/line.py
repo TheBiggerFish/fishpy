fdc64f8d762fd4d2fd3cc569d4a3ac997ce68221
TheBiggerFish <clhnumber4@gmail.com>
2021-12-05 15:18:45 -0500

    Updated Line class to inherit from Point2D Updated __str__ and __repr__ magic methods for Line class Renamed integer_points_along to lattice_points_along

--- geometry/line.py
+++ geometry/line.py
@@ -3,14 +3,16 @@
 from math import isclose
 from typing import Optional, Set
 
+from fishpy.geometry.lattice import LatticePoint
+
 from .linesegment import LineSegment
-from .point import Point
+from .point2d import Point2D
 
 
 class Line:
     """Class for storing and evaluating lines"""
 
-    def __init__(self,y_int:float,slope:float,vertical:bool=False,x_int:Optional[float]=None):
+    def __init__(self,y_int:Optional[float],slope:float,vertical:bool=False,x_int:Optional[float]=None):
         self.vertical = vertical
         self.x_int = x_int
         self.y_int = y_int
@@ -21,7 +23,14 @@ class Line:
             return f'x = {self.x_int}'
         slope = int(self.slope) if float(self.slope).is_integer() else self.slope
         y_int = int(self.y_int) if float(self.y_int).is_integer() else self.y_int
-        return f'y = {slope}x + {y_int}'
+
+        slope_str = f'{slope}x' if slope != 0 else ''
+        y_str = str(y_int) if y_int != 0 or slope == 0 else ''
+        add_str = ' + ' if slope_str and y_str else ''
+        return f'y = {slope_str}{add_str}{y_str}'
+
+    def __repr__(self) -> str:
+        return f'Line({self.slope=}, {self.y_int=}, {self.x_int=}, {self.vertical=})'
 
     @staticmethod
     def extend_segment(segment:LineSegment) -> 'Line':
@@ -37,7 +46,7 @@ class Line:
         """Returns the y-value of self at x-value"""
         return self.slope * x + self.y_int
 
-    def integer_points_along(self,lower_bound:Point,upper_bound:Point) -> Set[Point]:
+    def lattice_points_along(self,lower_bound:Point2D,upper_bound:Point2D) -> Set[LatticePoint]:
         """
         Find all lattice points lying within the rectangle created by
         lower_bound and upper_bound
@@ -46,7 +55,7 @@ class Line:
         points = set()
         if self.vertical and lower_bound.x <= self.x_int <= upper_bound.x:
             for y in range(lower_bound.y, upper_bound.y+1):
-                points.add(Point(self.x_int,y))
+                points.add(LatticePoint(self.x_int,y))
             return points
 
         for x in range(lower_bound.x,upper_bound.x+1):
@@ -54,10 +63,10 @@ class Line:
             if (self.slope < 0 and y < lower_bound.y) or (self.slope > 0 and y > upper_bound.y):
                 break
             if isclose(y - round(y),0,abs_tol=10**-4) and lower_bound.y <= y <= upper_bound.y:
-                points.add(Point(x,int(y)))
+                points.add(LatticePoint(x,int(y)))
         return points
 
-    def contains_point(self,point:Point) -> bool:
+    def contains_point(self,point:Point2D) -> bool:
         """Predicate function which returns whether point lies on self"""
         if self.vertical:
             return self.x_int == point.x
@@ -68,7 +77,7 @@ class Line:
         """Create a new vertical line at x"""
         return Line(None, float('inf'), True, x)
 
-    def perpendicular(self,intersection:Point) -> 'Line':
+    def perpendicular(self,intersection:Point2D) -> 'Line':
         """Returns a line which is perpendicular to self at a given point"""
 
         if not self.contains_point(intersection):
@@ -83,14 +92,14 @@ class Line:
         new_y_int = intersection.y - new_slope * intersection.x
         return Line(new_y_int,new_slope)
 
-    def reflect_point(self,p:Point) -> Point:
+    def reflect_point(self,p:Point2D) -> Point2D:
         """Reflect p across self"""
 
         if self.vertical:
-            return Point(2*self.x_int-p.x,p.y)
+            return Point2D(2*self.x_int-p.x,p.y)
         u=((1-self.slope**2)*p.x + 2*self.slope*p.y - 2*self.slope*self.y_int)/((self.slope**2)+1)
         v=((self.slope**2 - 1)*p.y + 2*self.slope*p.x + 2*self.y_int)/((self.slope**2) + 1)
-        return Point(u, v)
+        return Point2D(u, v)
 
     @property
     def y_int(self) -> float:
@@ -98,7 +107,7 @@ class Line:
         return self.__y_int
 
     @y_int.setter
-    def y_int(self,y_int:float):
+    def y_int(self,y_int:Optional[float]):
         if self.vertical and y_int is not None:
             raise ValueError('Cannot change y-intercept of vertical line')
         self.__y_int = y_int
