2e389006cf00ce01e4083772a1af2198f57e24a3
TheBiggerFish <clhnumber4@gmail.com>
2021-12-16 20:27:42 -0500

    Modified pathfinding.DepthFirst to a function Created shortest_path and longest_path functions

--- pathfinding/depthfirst.py
+++ pathfinding/depthfirst.py
@@ -1,58 +1,63 @@
 """Provides a class implementing a depth first search"""
 
-from typing import Callable, Generic, List, Optional, TypeVar, Union
+from typing import Callable, Dict, List, Optional, Tuple, TypeVar, Union
 
 from ..structures import Stack
 
 T = TypeVar('T')
 
-class DepthFirstTraversal(Generic[T]):
-    """A class implementing a depth first search"""
+def path(start:T,end:T,previous:Dict[T,T]):
+    path = []
+    cur = end
+    while cur != start:
+        path.append(cur)
+        cur = previous[cur]
+    path.append(cur)
+    return list(reversed(path))
 
-    def __init__(self, start:T, target:T,
-                 adjacency_function: Callable[[T],List[T]],
-                 distance_function: Optional[Callable[[T,T],Union[int,float]]] = None,
-                 validation_function: Optional[Callable[[T],bool]]  =None,
-                 cyclic: bool = False,
-                 longest_path: bool = False,
-                 verbose: bool = False):
-        if cyclic and longest_path:
-            raise ValueError('DepthFirstTraversal cannot be both cyclic and longest_path')
-        self.start = start
-        self.target = target
-        self._adjacency_function = adjacency_function
-        self._distance_function = distance_function
-        self._validation_function = validation_function
-        self.longest_path = longest_path
-        self.cyclic = cyclic
-        self.verbose = verbose
+def shortest_path(start:T,target:T,
+                  adjacency_function: Callable[[T],List[T]],
+                  distance_function: Optional[Callable[[T,T],Union[int,float]]] = None,
+                  validation_function: Optional[Callable[[T],bool]] = None
+                 ) -> Tuple[int,List[T]]:
+    """Execute a depth first search and return the length of shortest path to target"""
 
-        self.distance = {self.start:0}
-        self.prev = {self.start:None}
-        self.stack = Stack()
-        self.stack.push(self.start)
+    distance = {start:0}
+    prev = {}
 
-    def execute(self):
-        """Execute the depth first search and return the length of shortest path to target"""
+    stack = Stack()
+    stack.push(start)
 
-        while not self.stack.empty():
-            cur = self.stack.pop()
-            neighbors = self._adjacency_function(cur)
-            for adj in neighbors:
-                if self._validation_function is not None and not self._validation_function(adj):
-                    continue
-                if not self.cyclic and adj in self.prev:
-                    continue
+    while not stack.empty():
+        cur = stack.pop()
+        if cur == target:
+            return distance[target],path(start,target,prev)
+
+        neighbors = adjacency_function(cur)
+        for adj in neighbors:
+            if validation_function is not None and not validation_function(adj):
+                continue
+
+            if distance_function is not None:
+                d = distance_function(cur,adj)
+            elif adj in prev:
+                continue
+            else:
                 d = 1
-                if self._distance_function is not None:
-                    self._distance_function(cur,adj)
-                new_distance = self.distance[cur] + d
-                if (adj not in self.distance
-                    or (self.longest_path and self.distance[adj] < new_distance)
-                    or (not self.longest_path and self.distance[adj] > new_distance)):
 
-                    self.distance[adj] = new_distance
-                    self.prev[adj] = cur
-                    if not(self.longest_path and adj == self.target):
-                        self.stack.push(adj)
-        return self.distance[self.target]
+            new_distance = distance[cur] + d
+            if (adj not in distance
+                or (new_distance < distance[adj])):
+                distance[adj] = new_distance
+                prev[adj] = cur
+                stack.push(adj)
+    return None, None
+
+def longest_path(start:T,target:T,
+                  adjacency_function: Callable[[T],List[T]],
+                  distance_function: Optional[Callable[[T,T],Union[int,float]]] = None,
+                  validation_function: Optional[Callable[[T],bool]] = None
+                ) -> Tuple[int,List[T]]:
+    """Execute a depth first search and return the length of longest path to target"""
+
+    raise NotImplementedError('Longest path has not been implemented')
