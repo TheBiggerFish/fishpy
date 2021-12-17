e5052862a7b669bd01f3df588ac69110651c0dd8
TheBiggerFish <clhnumber4@gmail.com>
2021-12-16 20:28:48 -0500

    Remove depthfirst from pathfinding.__init__.py

--- pathfinding/__init__.py
+++ pathfinding/__init__.py
@@ -1,6 +1,5 @@
 """This module contains a collection of classes to aid in pathfinding"""
 
-from .depthfirst import DepthFirstTraversal
 from .dijkstra import Dijkstra
 from .location import Location
 from .location3d import Location3D
