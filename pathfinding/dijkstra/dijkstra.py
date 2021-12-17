06fef3ea8947f8b15933c582cb60be304611c7a3
TheBiggerFish <clhnumber4@gmail.com>
2021-12-16 20:31:39 -0500

    Expanded pathfinding.Dijkstra to allow movement cost

--- pathfinding/dijkstra/dijkstra.py
+++ pathfinding/dijkstra/dijkstra.py
@@ -5,7 +5,7 @@ Provides a class implementing Dijkstra's Algorithm with support of heuristics (A
 
 from functools import cached_property
 from queue import PriorityQueue
-from typing import Callable, Generic, List, Optional, TypeVar
+from typing import Callable, Dict, Generic, List, Optional, TypeVar
 
 from .dijkstraitem import DijkstraItem
 
@@ -17,14 +17,14 @@ class Dijkstra(Generic[T]):
     def __init__(self, start:T, target:T,
                  adjacency_function: Callable[[T],List[T]],
                  validation_function: Optional[Callable[[T],bool]] = None,
-                 heuristic_function: Optional[Callable[[T],int]] = None,
-                 verbose: bool = False):
+                 cost_function: Optional[Callable[[T],int]] = None,
+                 heuristic_function: Optional[Callable[[T,T],int]] = None):
         self.start = start
         self.target = target
         self._adjacency_function = adjacency_function
         self._validation_function = validation_function
+        self._cost_function = cost_function
         self._heuristic_function = heuristic_function
-        self.verbose = verbose
         self._dist = -1
 
         self.seen = set()
@@ -34,36 +34,42 @@ class Dijkstra(Generic[T]):
         estimate = DijkstraItem.get_h(self.start,self.target,self._heuristic_function)
         self.q.put(DijkstraItem(self.start,0,estimate))
 
-    def search(self, max_depth: int = -1) -> int:
+    def search(self, max_cost: Optional[int] = None) -> int:
         """Perform the shortest path search for the target"""
 
         if self.target in self.seen:
             return self._dist
+
+        g_scores:Dict[T,int] = {self.start:0}
+
         while not self.q.empty():
-            item = self.q.get()
-            if item.value in self.seen:
+            item:DijkstraItem = self.q.get()
+
+            if item.payload in self.seen:
                 continue
-            if self.verbose:
-                print(item.value,'\n')
+            self.seen.add(item.payload)
 
-            self.seen.add(item.value)
-
-            if item.value == self.target:
+            if item.payload == self.target:
                 self._dist = item.g
                 return item.g
 
-            g = item.g + 1
-            if g > max_depth > -1:
+            if max_cost is not None and item.g > max_cost:
                 continue
-            for adj in self._adjacency_function(item.value):
+            if self._cost_function is None:
+                g = item.g + 1
+
+            for adj in self._adjacency_function(item.payload):
                 if self._validation_function is None or self._validation_function(adj):
-                    if adj not in self.seen and adj not in self.prev:
+                    if self._cost_function is not None:
+                        g = item.g + self._cost_function(item.payload,adj)
+                    if g < g_scores.get(adj,float('inf')):
+                        g_scores[adj] = g
+                        self.prev[adj] = item.payload
                         h = DijkstraItem.get_h(adj,self.target,self._heuristic_function)
                         self.q.put(DijkstraItem(adj,g,h))
-                        self.prev[adj] = item.value
         return -1
 
-    @cached_property
+    @property
     def path(self) -> List[T]:
         """
         Return the path taken from start to the target
