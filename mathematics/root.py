09e1523c9aff52d457e6c50f48a3fcb2c5cd9721
TheBiggerFish <clhnumber4@gmail.com>
2022-01-16 20:36:54 -0500

    Rewrote IntegerSquareRoots class as a function

--- mathematics/root.py
+++ mathematics/root.py
@@ -1,36 +1,30 @@
 """Provides a class for calculating integer square roots"""
 
-from typing import List
+
+def _get_pairs(n: int):
+    string = str(n)
+    return [int(string[i:i+2]) for i in range(0, len(string), 2)]
 
 
-class IntegerSquareRoot:
-    """A class for calculating integer square roots"""
+def integer_square_root(n: int, precision: int) -> list[int]:
+    """A function for calculating integer square roots"""
 
-    def __init__(self,n:int,precision:int):
-        self.n = n
-        self.precision = precision
-        self.offset = len(str(self.n)) + len(str(self.n))%2
-        self.value = []
-        self.__calculate()
+    n = n
+    precision = precision
+    offset = len(str(n)) + len(str(n)) % 2
+    value = []
 
-    def __get_pairs(self) -> List[str]:
-        string = str(self.n)
-        if self.offset%2 == 1:
-            string = '0' + string
-        return [int(string[i:i+2]) for i in range(0,len(string),2)]
-
-    # Uses the algorithm described here:
-    #   https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Decimal_(base_10)
-    def __calculate(self) -> None:
-        pairs = self.__get_pairs()
-        c,p = 0,0
-        for i in range(self.precision):
-            c = c*100 + 0 if i >= len(pairs) else pairs[i]
-            x = 0
-            while x * (20*p + x) <= c:
-                x += 1
-            x -= 1
-            self.value.append(x)
-            y = x * (20*p + x)
-            p = p * 10 + x
-            c -= y
+    #  https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Decimal_(base_10)
+    pairs = _get_pairs()
+    c, p = 0, 0
+    for i in range(precision):
+        c = c*100 + 0 if i >= len(pairs) else pairs[i]
+        x = 0
+        while x * (20*p + x) <= c:
+            x += 1
+        x -= 1
+        value.append(x)
+        y = x * (20*p + x)
+        p = p * 10 + x
+        c -= y
+    return value
