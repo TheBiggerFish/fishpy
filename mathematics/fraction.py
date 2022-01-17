7eb64f4d685b54ace7b9f1701a360dd507a7af6a
TheBiggerFish <clhnumber4@gmail.com>
2022-01-16 20:30:26 -0500

    Updated formatting for PEP8 and added __repr__ methods

--- mathematics/fraction.py
+++ mathematics/fraction.py
@@ -9,7 +9,7 @@ from typing import Optional, Sequence
 class Fraction:
     """Class to be used in storing and evaluating fractions"""
 
-    def __init__(self,numer,denom=1):
+    def __init__(self, numer: int, denom: int = 1):
         self.n = numer
         self.d = denom
 
@@ -23,44 +23,47 @@ class Fraction:
 
     def reciprocal(self) -> 'Fraction':
         """Return the reciprocal fraction of self"""
-        return Fraction(self.d,self.n)
+        return Fraction(self.d, self.n)
 
-    def add_int(self,num:int) -> 'Fraction':
+    def add_int(self, num: int) -> 'Fraction':
         """Add an integer value to the fraction"""
-        return Fraction(self.n + (num * self.d),self.d)
+        return Fraction(self.n + (num * self.d), self.d)
 
     def reduce(self) -> 'Fraction':
         """Reduce the fraction to the lowest equivalent value"""
-        div = gcd(self.n,self.d)
-        return Fraction(self.n//div,self.d//div)
+        div = gcd(self.n, self.d)
+        return Fraction(self.n//div, self.d//div)
 
     @staticmethod
-    def _lcd(f1:'Fraction',f2:'Fraction'):
-        return Fraction._lcm(f1.d,f2.d)
+    def _lcd(f1: 'Fraction', f2: 'Fraction'):
+        return Fraction._lcm(f1.d, f2.d)
 
     @staticmethod
-    def _lcm(a:int,b:int) -> int:
-        return abs(a*b) // gcd(a,b)
+    def _lcm(a: int, b: int) -> int:
+        return abs(a*b) // gcd(a, b)
 
-    def __mul__(self,other:'Fraction') -> 'Fraction':
-        return Fraction(self.n*other.n,self.d*other.d)
+    def __mul__(self, other: 'Fraction') -> 'Fraction':
+        return Fraction(self.n*other.n, self.d*other.d)
 
-    def __add__(self,other:'Fraction') -> 'Fraction':
-        lcd = Fraction._lcd(self,other)
+    def __add__(self, other: 'Fraction') -> 'Fraction':
+        lcd = Fraction._lcd(self, other)
         self_n = self.n * (lcd // self.d)
         other_n = other.n * (lcd // other.d)
-        return Fraction(self_n + other_n,lcd)
+        return Fraction(self_n + other_n, lcd)
 
     def __str__(self) -> str:
         return str(self.n) + '/' + str(self.d)
 
-    def __lt__(self,other:'Fraction') -> bool:
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(n={self.n},d={self.d})'
+
+    def __lt__(self, other: 'Fraction') -> bool:
         return self.evaluate() < other.evaluate()
 
-    def __gt__(self,other:'Fraction') -> bool:
+    def __gt__(self, other: 'Fraction') -> bool:
         return self.evaluate() > other.evaluate()
 
-    def __eq__(self,other:'Fraction') -> bool:
+    def __eq__(self, other: 'Fraction') -> bool:
         return self.evaluate() == other.evaluate()
 
     def __hash__(self) -> int:
@@ -68,17 +71,23 @@ class Fraction:
             return self.n * self.n + self.n + self.d
         return self.d * self.d + self.n
 
+
 class ContinuedFraction:
     """Class for storing continued fractions"""
 
-    def __init__(self,addend:int=0,numer:int=0,
-                 denom:Optional['ContinuedFraction']=None):
+    def __init__(self, addend: int = 0, numer: int = 0,
+                 denom: Optional['ContinuedFraction'] = None):
         self.a = addend
         self.n = numer
         self.d = denom
 
+    def __repr__(self) -> str:
+        denom = self.d.__class__.__name__ if self.d is not None else None
+        return (f'{self.__class__.__name__}(addend={self.a},'
+                f'numerator={self.n},denominator={denom})')
+
     @staticmethod
-    def gen_from_seq(seq:Sequence[int],x:int=2) -> 'ContinuedFraction':
+    def gen_from_seq(seq: Sequence[int], x: int = 2) -> 'ContinuedFraction':
         """
         Generate a continued fraction from a sequence of integers
         The value x is used to generate the numerators of the continued fraction
@@ -89,7 +98,7 @@ class ContinuedFraction:
 
         frac = ContinuedFraction(addend=seq[-1])
         for num in reversed(seq[:-1]):
-            if not isinstance(num,int):
+            if not isinstance(num, int):
                 raise TypeError
             frac.d = frac.copy()
             frac.n = x-1
@@ -106,7 +115,7 @@ class ContinuedFraction:
             frac = frac.d
         return seq
 
-    def append(self,frac:'ContinuedFraction') -> 'ContinuedFraction':
+    def append(self, frac: 'ContinuedFraction') -> 'ContinuedFraction':
         """Append continued fraction to the last continued fraction"""
 
         recurse = self
@@ -130,7 +139,7 @@ class ContinuedFraction:
         return sub_fraction.add_int(self.a)
 
     @staticmethod
-    def gen_sqrt_seq(n:int,l:int) -> Sequence[int]:
+    def gen_sqrt_seq(n: int, l: int) -> Sequence[int]:
         """
         Generate a continued fraction representation sequence of length l for
         a square root of number n
@@ -151,7 +160,7 @@ class ContinuedFraction:
 
     def copy(self) -> 'ContinuedFraction':
         """Returns a shallow copy of self"""
-        return ContinuedFraction(addend=self.a,numer=self.n,denom=self.d)
+        return ContinuedFraction(addend=self.a, numer=self.n, denom=self.d)
 
     @property
     def a(self) -> int:
@@ -172,17 +181,17 @@ class ContinuedFraction:
         return self.__d
 
     @a.setter
-    def a(self, a:int) -> None:
-        if not isinstance(a,int) and not isinstance(a,float):
+    def a(self, a: int) -> None:
+        if not isinstance(a, int) and not isinstance(a, float):
             raise TypeError
         self.__a = a
 
     @n.setter
-    def n(self, n:int) -> None:
-        if not isinstance(n,int) and not isinstance(n,float):
+    def n(self, n: int) -> None:
+        if not isinstance(n, int) and not isinstance(n, float):
             raise TypeError
         self.__n = n
 
     @d.setter
-    def d(self, d:'ContinuedFraction') -> None:
+    def d(self, d: 'ContinuedFraction') -> None:
         self.__d = d
