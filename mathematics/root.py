165ebb370e1d72d3c7d9b060afc9a7a1566f302c
TheBiggerFish <clhnumber4@gmail.com>
2022-01-16 21:32:10 -0500

    Correcting linting errors in root.py

--- mathematics/root.py
+++ mathematics/root.py
@@ -1,21 +1,22 @@
 """Provides a class for calculating integer square roots"""
 
+from typing import List
+
 
 def _get_pairs(n: int):
     string = str(n)
+    if len(string) % 2 != 0:
+        string = '0' + string
     return [int(string[i:i+2]) for i in range(0, len(string), 2)]
 
 
-def integer_square_root(n: int, precision: int) -> list[int]:
+def integer_square_root(n: int, precision: int) -> List[int]:
     """A function for calculating integer square roots"""
-
-    n = n
-    precision = precision
-    offset = len(str(n)) + len(str(n)) % 2
-    value = []
-
     #  https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Decimal_(base_10)
+
+    value = []
     pairs = _get_pairs()
+
     c, p = 0, 0
     for i in range(precision):
         c = c*100 + 0 if i >= len(pairs) else pairs[i]
