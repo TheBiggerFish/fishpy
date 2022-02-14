185bb0e61544a4268d0aa5defc385fde1f44d0f9
TheBiggerFish <clhnumber4@gmail.com>
2022-02-14 15:23:34 -0500

    Updated formatting for structures.range, added iterator

--- structures/range.py
+++ structures/range.py
@@ -1,6 +1,6 @@
 """This module provides a class to represent a range of numbers"""
 
-from typing import Iterable, Union
+from typing import Union
 
 from .bound import Bounds
 
@@ -11,41 +11,49 @@ class Range:
     interact with the range
     """
 
-    def __init__(self,lower:Union[int,float],
-                 upper:Union[int,float],
-                 upper_inclusive:bool=False):
-        self.bound = Bounds(lower,upper,upper_inclusive)
+    def __init__(self, lower: Union[int, float],
+                 upper: Union[int, float],
+                 upper_inclusive: bool = False):
+        self.bound = Bounds(lower, upper, upper_inclusive)
+        self._max = upper + 1 if upper_inclusive else upper
+        self._cur = lower
 
     @staticmethod
-    def from_string(string:str,upper_inclusive:bool=False) -> 'Range':
+    def from_string(string: str, upper_inclusive: bool = False) -> 'Range':
         """Generate a range from a string in format (low-high)"""
 
         split = string.split('-')
-        l,r = int(split[0]),int(split[1])
-        return Range(l,r,upper_inclusive)
+        l, r = int(split[0]), int(split[1])
+        return Range(l, r, upper_inclusive)
 
-    def size(self) -> Union[int,float]:
+    def size(self) -> Union[int, float]:
         """Return the size of the range"""
         offset = (1 if self.bound.upper.inclusive else 0) - 1
         return self.bound.upper - self.bound.lower + offset
 
-    def __len__(self) -> Union[int,float]:
+    def step(self, current_integer: int, step_size: int = 1) -> int:
+        next_int = current_integer + step_size
+        if next_int not in self:
+            next_int = self.bound.lower.value
+        return next_int
+
+    def __len__(self) -> Union[int, float]:
         return self.size()
 
-    def __lt__(self,other:'Range') -> bool:
+    def __lt__(self, other: 'Range') -> bool:
         return self.bound.lower < other.bound.lower
 
-    def __gt__(self,other:'Range') -> bool:
+    def __gt__(self, other: 'Range') -> bool:
         return self.bound.lower > other.bound.lower
 
     def __str__(self) -> str:
         rchar = ']' if self.bound.upper.inclusive else ')'
         return f'[{self.bound.lower}-{self.bound.upper}{rchar}'
 
-    def __contains__(self,item) -> bool:
+    def __contains__(self, item) -> bool:
         return item in self.bound
 
-    def division(self,parts:int,which:int) -> 'Range':
+    def division(self, parts: int, which: int) -> 'Range':
         """
         Return a section of the range by splitting the range into parts and
         selecting one
@@ -58,13 +66,20 @@ class Range:
             step = int(step)
         lower = self.bound.lower + (step * which)
         upper = lower + step
-        return Range(lower,upper)
+        return Range(lower, upper)
 
-    def __iter__(self) -> Iterable[Union[int,float]]:
-        if not isinstance(self.bound.lower,int):
-            raise TypeError(f'\'{type(self.bound.lower)}\' cannot be interpreted as an integer')
-        if not isinstance(self.bound.upper,int):
-            raise TypeError(f'\'{type(self.bound.upper)}\' cannot be interpreted as an integer')
-        upper = self.bound.upper+1 if self.bound.upper.inclusive else self.bound.upper
-        for i in range(self.bound.lower,upper):
-            yield i
+    def __iter__(self):
+        if not isinstance(self.bound.lower, int):
+            raise TypeError(
+                f'\'{type(self.bound.lower.value)}\' cannot be interpreted as an integer')
+        if not isinstance(self.bound.upper, int):
+            raise TypeError(
+                f'\'{type(self.bound.upper.value)}\' cannot be interpreted as an integer')
+        return self
+
+    def __next__(self) -> int:
+        if self._cur >= self.bound.upper:
+            raise StopIteration
+        value = self._cur
+        self._cur += 1
+        return value
