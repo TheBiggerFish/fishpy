71c8eb8e87f9f282de9b6938709968f54b562855
TheBiggerFish <clhnumber4@gmail.com>
2022-12-09 01:08:02 -0500

    Implemented item get/set methods for Point2D

--- geometry/d2/point2d.py
+++ geometry/d2/point2d.py
@@ -1,6 +1,6 @@
 """This module provides a class for storing points on an x-y plane"""
 
-from typing import Final
+from typing import Final, Union
 
 from ..point import Point
 from .vector2d import Vector2D
@@ -19,6 +19,22 @@ class Point2D(Point):
 
     def __hash__(self) -> int:
         return hash(tuple(self._coords))
+
+    def __getitem__(self, key: Union[int, slice]) -> float:
+        if isinstance(key, int):
+            if key > 1:
+                raise IndexError(
+                    f'{self.__class__.__name__} supports only 2 dimensions')
+        return super().__getitem__(key)
+
+    def __setitem__(self, index: int, value: float) -> None:
+        if not isinstance(index, int):
+            raise TypeError('Dimension index must be an integer')
+
+        if index > 1:
+            raise IndexError(
+                f'{self.__class__.__name__} supports only 2 dimensions')
+        return super().__setitem__(index, value)
 
     def is_above(self, other: 'Point2D') -> bool:
         """Predicate function which returns whether self is above other"""
